c1597c91c5236020a9e2e93b3c17adbeda44ae14
Jasenko <121662342+jsnko@users.noreply.github.com>
2023-03-15 18:15:26 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+function orderFromBoss() {
+  return new Promise(function(resolve, reject) {
+    const isCodeDone = false;
+    console.log("Finish the code, it's an order!");
+    resolve(isCodeDone);
+  });
+}
+
+function coding(isCodeDone) {
+  return new Promise(function(resolve, reject){
+    if(isCodeDone === false) {
+      console.log("Working...");
+      isCodeDone = true;
+      resolve(isCodeDone);
+    } else {
+      reject("No instructions from boss yet");
+    }  
+  });
+}
+
+function performanceReview(isCodeDone) {
+  return new Promise(function(resolve, reject){
+    if(isCodeDone) {
+      resolve("You kept your promise and finished the code. You're getting promoted!");
+    } else {
+      reject("You broke your promise, how can I trust you with bigger responsibilities?");
+    }
+  });
+}
+
+
+async function randomDayAtWork() {
+  try {
+    const instruction = await orderFromBoss();
+    const work = await coding(instruction);
+    const review = await performanceReview(work);
+    console.log(review);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+randomDayAtWork();
